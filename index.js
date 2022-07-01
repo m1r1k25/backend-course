5fc7688ec62e3b4967db6d66e513e198362cfbe4
m1r1k77 <m1r1k25@mail.ru>
2022-07-01 22:48:14 +0300

    feat: add routers and controlers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import Post from './Post.js'
+import router from './router.js'
 
 const PORT = 5000
 
@@ -9,16 +9,7 @@ const DB_URL = `mongodb+srv://user111:user111@clustera.ujgufi5.mongodb.net/?retr
 const app = express()
 
 app.use(express.json())
-
-app.post('/', async (req, res) => {
-  try {
-    const {author, title, content, picture} = req.body
-    const post = await Post.create({author, title, content, picture})
-    res.json(post)
-  } catch(e) {
-      res.status(500).json(e.message)
-  }
-})
+app.use('/api', router)
 
 async function startApp() {
   try {
